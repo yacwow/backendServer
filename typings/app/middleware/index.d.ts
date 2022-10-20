278e5a373b1a91e8c19d05a8298d3bd9c556ed87
haochen <haochenbc@gmail.com>
2022-10-20 17:44:06 +0800

    upload

---
+++ typings/app/middleware/index.d.ts
@@ -0,0 +1,19 @@
+// This file is created by egg-ts-helper@1.33.0
+// Do not modify this file!!!!!!!!!
+
+import 'egg';
+import ExportAuth = require('../../../app/middleware/auth');
+import ExportCaptcha = require('../../../app/middleware/captcha');
+import ExportLimit = require('../../../app/middleware/limit');
+import ExportPassport = require('../../../app/middleware/passport');
+import ExportResponseFomatter = require('../../../app/middleware/responseFomatter');
+
+declare module 'egg' {
+  interface IMiddleware {
+    auth: typeof ExportAuth;
+    captcha: typeof ExportCaptcha;
+    limit: typeof ExportLimit;
+    passport: typeof ExportPassport;
+    responseFomatter: typeof ExportResponseFomatter;
+  }
+}
